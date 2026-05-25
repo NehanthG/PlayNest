b50f4d2be865b180a35539331c92ab255565755a
Utej <91786744+utej8553@users.noreply.github.com>
2026-05-25 17:22:17 +0530

    modified urls

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/playnest")
+        const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://mongo-service:27017/playnest")
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
